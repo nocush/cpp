b6a846357469751aa48d1fbb9d1641e89712177a
Mat <106345025+nocush@users.noreply.github.com>
2022-10-17 12:29:44 +0200

    .

--- Lab2_2/Lab2_2/Lab2_2.cpp
+++ Lab2_2/Lab2_2/Lab2_2.cpp
@@ -13,17 +13,19 @@ int main()
 		tab[i]->setBonus(i+1);
 		tab[i]->show();
 	}*/
-	int a = 3;
+	int a = 4;
 	Employee* e[4];
-	e[3] = new Developer("Nowak", 30, 7, 4000, 3);
-	e[2] = new TeamLeader("Kowalski", 40, 15, 6000, 2);
-	e[1] = new Developer("Bartczak", 25, 4, 3500, 2);
-	e[0] = new Developer("Marchewka", 23, 3, 3500, 3);
+	e[0] = new Developer("Nowak", 30, 7, 4000, 3);
+	e[1] = new TeamLeader("Kowalski", 40, 15, 6000, 2);
+	e[2] = new Developer("Bartczak", 25, 4, 3500, 2);
+	e[3] = new Developer("Marchewka", 23, 3, 3500, 3);
 	e[3]->show();
 	e[2]->show();
 	e[1]->show();
 	e[0]->show();
+	cout << "=========================================================================" << endl;
 	whoWorkMoreThan5Years(e, a);
+	cout << "=========================================================================" << endl;
 	howManyEarnLessThanMeanBonus(e, a);
 
 	return 0;
